620d63df1a4f51ad245324d68758b8f25f2f2855
Lorenzo91347 <lorenzo.losito91@gmail.com>
2023-11-08 10:53:37 +0100

    feat:correcting

--- Main.js
+++ Main.js
@@ -1,1 +1,24 @@
 'use strict'
+
+const pKm = document.getElementById('km');
+const pAge = document.getElementById('age');
+const pGen = document.getElementById('gen');
+const pCanc = document.getElementById('canc');
+
+console.log(pKm);
+console.log(pAge);
+console.log(pGen);
+
+pGen.addEventListener('click', function () {
+    console.log('button click');
+    console.log(pKm);
+    console.log(pAge);
+
+});
+pCanc.addEventListener('click', function () {
+    pKm = 'null'
+    pAge = 'null'
+
+});
+
+
